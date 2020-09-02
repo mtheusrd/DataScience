09c52461bf9d2e64c39a1bea02feaa040416ede9
Matheus Rodrigues <67073149+mtheusrd@users.noreply.github.com>
2020-09-02 19:09:27 -0300

    Update Calculadora_Theuzin.py

--- Calculadora_Theuzin.py
+++ Calculadora_Theuzin.py
@@ -1,4 +1,5 @@
 # Calculadora do Theuzin
+
 # Menu Inicial
 print('**************************************************')
 print('*       BEM VINDO A CALCULADORA DO THEUZIN       *')
@@ -14,26 +15,32 @@ print('************************************************** \n \n')
 # Escolha da Opção
 opcao = int(input('Qual operação você deseja fazer?  '))
 
+# Adição
 if opcao == 1:
 	num1 = int(input('Digite o primeiro número: ')) 
 	num2 = int(input('Digite o segundo número: '))
 	total = num1 + num2
 	print('O Resultado final foi : %d' %total)
+# Subtração
 elif opcao == 2:
 	num1 = int(input('Digite o primeiro número: ')) 
 	num2 = int(input('Digite o segundo número: '))
 	total = num1 - num2
 	print('O Resultado final foi : %d' %total)
+# Divisão
 elif opcao == 3:
 	num1 = float(input('Digite o primeiro número: ')) 
 	num2 = float(input('Digite o segundo número: '))
 	total = num1 / num2
 	print('O Resultado final foi : %.2f' %total)
+# Multiplicação
 elif opcao == 4:
 	num1 = int(input('Digite o primeiro número: ')) 
 	num2 = int(input('Digite o segundo número: '))
 	total = num1 * num2
 	print('O Resultado final foi : %d' %total)
+# Sair do Programa
 elif opcao == 0:
 	exit()
+# Caso o usuário escolha alguma opção que não está listada
 else: print('Opção Inválida! ')
